f27e68a00764b92007ea4b7710806d43213df0c1
pktjesus1 <pktjesus@gmail.com>
2025-02-13 00:07:12 +0900

    webapp 커스텀 에러 처리2 및 권한 처리2

---
+++ gidosa-full-webapp/src/main/java/net/gidosa/full/webapp/controllers/CustomErrorController.java
@@ -0,0 +1,36 @@
+package net.gidosa.full.webapp.controllers;
+
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Log4j2
+@Controller
+public class CustomErrorController implements org.springframework.boot.web.servlet.error.ErrorController {
+
+    @RequestMapping("/error")
+    public String handleError(HttpServletRequest request, Model model) {
+        Object status = request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE);
+        Object exception = request.getAttribute(RequestDispatcher.ERROR_EXCEPTION);
+
+        model.addAttribute("headerInvisible", true);
+
+        if (status != null) {
+            int statusCode = Integer.parseInt(status.toString());
+
+            if (statusCode == HttpStatus.NOT_FOUND.value()) {
+                log.error("404 Error occurred. Path: {}", request.getAttribute(RequestDispatcher.ERROR_REQUEST_URI));
+                return "error/404";
+            } else if (statusCode == HttpStatus.INTERNAL_SERVER_ERROR.value()) {
+                log.error("500 Error occurred with exception:", exception);
+                return "error/500";
+            }
+        }
+
+        return "error/500";  // 기본적으로 500 에러 페이지로 리다이렉트
+    }
+}
