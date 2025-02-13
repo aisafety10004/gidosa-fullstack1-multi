6c2b3b2a0cd30cb09121fce8a428543dace941e9
pktjesus <pktjesus@gmail.com>
2025-02-13 17:13:33 +0900

    1. github ci-cd webapp 추가1

---
+++ gidosa-webapp-api/scripts/code-deploy/start-dev-inner.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+WEBAPP_BASE_FOLDER=/home/gapture/apps-gidosa
+WEBAPP_BACKEND_FOLDER=gidosa-webapp-backend
+WEBAPP_FOLDER="$WEBAPP_BASE_FOLDER/$WEBAPP_BACKEND_FOLDER"
+
+WEBAPP_API_NAME=gidosa-webapp-api
+WEBAPP_API_FULL="$WEBAPP_FOLDER/$WEBAPP_API_NAME"
+
+WEBAPP_API_LOGS_FOLDER=logs-backends-all
+WEBAPP_API_LOGS_FOLDER_FULL="$WEBAPP_BASE_FOLDER/$WEBAPP_API_LOGS_FOLDER"
+
+DEPLOY_LOG_NAME="$WEBAPP_API_NAME-deploy.log"
+DEPLOY_LOG_FULL="$WEBAPP_API_FULL-deploy.log"
+
+PROFILE=$(cat ${WEBAPP_FOLDER}/profile)
+#PINPOINT_AGENT_FILENAME=pinpoint-bootstrap-2.5.3.jar
+#PINPOINT_AGENT=./agents-dependency/pinpoint/pinpoint-bootstrap-2.5.3.jar
+#PINPOINT_CONFIG=./agents-dependency/pinpoint/pinpoint-root.config
+
+cp -f ${WEBAPP_FOLDER}/gidosa-webapp-backend/build/libs/${WEBAPP_API_NAME}.jar ${WEBAPP_FOLDER}/${WEBAPP_API_NAME}.jar
+
+APP_LOG="$WEBAPP_API_LOGS_FOLDER_FULL/$WEBAPP_API_NAME.log"
+ERROR_LOG="$WEBAPP_API_LOGS_FOLDER_FULL/$WEBAPP_API_NAME-error.log"
+
+cd ${WEBAPP_FOLDER}
+
+echo "gidosa-full-webapp starting"
+#nohup java -jar -javaagent:${PINPOINT_AGENT} -Dpinpoint.applicationName=${PINPOINT_APPLICATION_NAME} -Dpinpoint.agentId=${PINPOINT_AGENT_ID} -Dpinpoint.config=${PINPOINT_CONFIG} -Dspring.profiles.active=${PROFILE} -Duser.timezone=Asia/Seoul ${WEBAPP_API_NAME}.jar > $APP_LOG 2> $ERROR_LOG &
+nohup /home/gapture/Downloads/jdk-21/bin/java -agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=*:8120 -jar gidosa-webapp-api.jar > /home/gapture/apps-gidosa/logs-backends-all/gidosa-webapp-api.log 2> /home/gapture/apps-gidosa/logs-backends-all/gidosa-webapp-api-error.log &
+sleep 10       # pid파일이 만들어지기까지 기다리는 예상시간
+
+WEBAPP_API_PID=$(cat $WEBAPP_API_NAME.pid)
+TIME_NOW=$(date +%c)
+
+echo "$TIME_NOW > 실행된 프로세스 아이디는 $WEBAPP_API_PID 입니다." >> $DEPLOY_LOG_FULL
---
+++ gidosa-webapp-api/scripts/code-deploy/stop-dev-inner.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+WEBAPP_BASE_FOLDER=/home/gapture/apps-gidosa
+WEBAPP_BACKEND_FOLDER=gidosa-webapp-backend
+WEBAPP_FOLDER="$WEBAPP_BASE_FOLDER/$WEBAPP_BACKEND_FOLDER"
+
+WEBAPP_API_NAME=gidosa-webapp-api
+WEBAPP_API_FULL="$WEBAPP_FOLDER/$WEBAPP_API_NAME"
+
+# 현재 구동 중인 애플리케이션 pid 확인
+WEBAPP_API_PID_FILENAME=${WEBAPP_API_FULL}.pid
+
+#cd ${WEBAPP_FOLDER}
+
+TIME_NOW=$(date +%c)
+DEPLOY_LOG="$WEBAPP_API_FULL-deploy.log"
+
+# 프로세스가 켜져 있으면 종료(pid파일이 있으면)
+if [ -f "$WEBAPP_API_PID_FILENAME" ]; then
+  WEBAPP_API_PID=$(cat $WEBAPP_API_PID_FILENAME)
+
+  kill -15 $WEBAPP_API_PID
+  echo "$TIME_NOW > 실행중인 PID $WEBAPP_API_PID $WEBAPP_API_NAME.jar 애플리케이션 종료 " >> $DEPLOY_LOG
+else
+  echo "$TIME_NOW > 현재 실행중인 애플리케이션이 없습니다" >> $DEPLOY_LOG
+fi
