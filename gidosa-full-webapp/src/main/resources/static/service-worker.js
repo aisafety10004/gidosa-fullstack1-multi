ac9a0e3760d57e831da5d00b2b9034a2cf6e7aa4
pktjesus <pktjesus@gmail.com>
2025-04-07 16:02:02 +0900

    웹앱 Firebase 및 푸시 알림 기능 추가
    - build.gradle 파일에 Firebase 및 웹 푸시 관련 의존성 추가.
    - service-worker.js 파일에 푸시 이벤트 리스너 추가 및 로그 출력 기능 구현.
    - firebase-app-compat961.js 및 firebase-messaging-compat961.js 파일을 새로 추가하여 Firebase 기능 호환성 제공.
    - footer-general-sub.html 파일에서 푸시 알림 관련 주석 추가 및 설명 보강.
    - general.html 파일에 Firebase 관련 스크립트 추가하여 전체 페이지에서 Firebase 기능 사용 가능하도록 수정.

--- gidosa-full-webapp/src/main/resources/static/service-worker.js
+++ gidosa-full-webapp/src/main/resources/static/service-worker.js
@@ -6,3 +6,18 @@ self.addEventListener('fetch', function (e) {
   // 캐시 전략 설정 가능
   //console.log('Service Worker fetch.............');
 });
+
+// VAPID용???
+self.addEventListener('push', function(e) {
+  console.log('Service Worker push.............', e.data);
+  
+  // const data = e.data.json();
+  // const options = {
+  //   body: data.body,
+  //   icon: '/assets/images/baroceum_app_192x192.png',
+  //   badge: '/assets/images/baroceum_app_72x72.png'
+  // };
+  // e.waitUntil(
+  //   self.registration.showNotification(data.title, options)
+  // );
+});
