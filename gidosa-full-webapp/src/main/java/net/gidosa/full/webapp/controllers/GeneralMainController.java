cf54e605d7a0c6cee517e30ec8ffe39f3fa8b4b3
KitaePark <pktjesus@gmail.com>
2025-04-12 12:29:28 +0900

    웹앱 메인 페이지 제목 및 컨트롤러 수정
    - GeneralMainController.java 파일에서 사용자의 건설 정보를 가져와 모델에 추가.
    - main.html 및 main2.html 파일에서 페이지 제목을 '현장공사 - 바로세움'으로 변경하여 일관성 유지.
    - 변경된 파일: GeneralMainController.java, main.html, main2.html.

--- gidosa-full-webapp/src/main/java/net/gidosa/full/webapp/controllers/GeneralMainController.java
+++ gidosa-full-webapp/src/main/java/net/gidosa/full/webapp/controllers/GeneralMainController.java
@@ -5,6 +5,7 @@ import lombok.extern.log4j.Log4j2;
 import net.gidosa.full.webapp.configs.auth.PrincipalDetails;
 import net.gidosa.full.webapp.services.GeneralMainService;
 import net.gidosa.full.webapp.services.GeneralConstructionService;
+import net.gidosa.rdb.models.entities.dbs.mysql.Construction;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -22,11 +23,21 @@ public class GeneralMainController {
 
     @GetMapping("/main")
     public String index(Model model, @AuthenticationPrincipal PrincipalDetails principalDetails) {
+        Long constructionId = principalDetails.getMemberGeneral().getConstruction().getId();
+
+        Construction construction = generalConstructionService.getConstruction(constructionId);
+        model.addAttribute("construction", construction);
+
         return "pages/general/main/main";
     }
 
     @GetMapping("/main2")
     public String index2(Model model, @AuthenticationPrincipal PrincipalDetails principalDetails) {
+        Long constructionId = principalDetails.getMemberGeneral().getConstruction().getId();
+
+        Construction construction = generalConstructionService.getConstruction(constructionId);
+        model.addAttribute("construction", construction);
+
         return "pages/general/main/main2";
     }
 }
