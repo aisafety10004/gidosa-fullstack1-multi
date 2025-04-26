af937a8cdc9b73ec8aa484ba019500e5aecc65a8
pktjesus1 <pktjesus@gmail.com>
2025-04-27 01:30:44 +0900

    - gidosa-full-webapp/src/main/java/net/gidosa/full/webapp/controllers/HtmlViewerController.java 파일에서 사용자 HTML 뷰어 기능을 개선하고, 로그인 상태에 따라 현장 정보 및 메뉴 데이터를 모델에 추가함. - gidosa-full-webapp/src/main/resources/templates/custom-viewer/html-view.html 파일에서 사용자 HTML 삽입 위치를 수정하고, 레이아웃 구조를 개선함.

--- gidosa-full-webapp/src/main/java/net/gidosa/full/webapp/controllers/HtmlViewerController.java
+++ gidosa-full-webapp/src/main/java/net/gidosa/full/webapp/controllers/HtmlViewerController.java
@@ -1,7 +1,17 @@
 package net.gidosa.full.webapp.controllers;
 
+import lombok.RequiredArgsConstructor;
 import net.gidosa.common.constants.CommonConsts;
+import net.gidosa.full.webapp.configs.auth.PrincipalDetails;
+import net.gidosa.full.webapp.services.GeneralConstructionService;
+import net.gidosa.full.webapp.services.GeneralCustomHtmlService;
+import net.gidosa.full.webapp.services.GeneralCustomMenuService;
+import net.gidosa.rdb.models.entities.dbs.mysql.Construction;
+import net.gidosa.rdb.models.entities.dbs.mysql.CustomMenu;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,18 +23,36 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 @Controller
+@RequiredArgsConstructor
 @RequestMapping("/custom-viewer/htmls")
 public class HtmlViewerController {
+    private final GeneralConstructionService generalConstructionService;
+    private final GeneralCustomMenuService generalCustomMenuService;
 
     @Value("${file.upload.path}")
     private String uploadDir;
 
     @GetMapping("/{fileId}")
-    public String viewUserHtml(@PathVariable String fileId, Model model) throws IOException {
+    public String viewUserHtml(@PathVariable String fileId, Model model, @AuthenticationPrincipal PrincipalDetails principalDetails) throws IOException {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        boolean isLoggedIn = authentication != null && authentication.isAuthenticated() &&
+                !authentication.getName().equals("anonymousUser");
+
+        if (isLoggedIn) {
+            Long constructionId = principalDetails.getMemberGeneral().getConstruction().getId();
+            Construction construction = generalConstructionService.getConstruction(constructionId);
+            model.addAttribute("construction", construction);
+        }
+
+        // 현장에 해당하는 메뉴 데이터 로드
+        List<CustomMenu> customMenus = generalCustomMenuService.getAllMenusByConstructionId(constructionId);
+        model.addAttribute("customMenus", customMenus);
+
         //final Path uploadPath = Paths.get("uploaded-html");
 //        final Path uploadPath = Paths.get(uploadDir);
 //        Path filePath = uploadPath.resolve(fileId + ".html");
